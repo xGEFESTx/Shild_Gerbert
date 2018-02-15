b163874989aea7b1a1482b4d836b22da7edcaf5a
xGEFESTx <sh.genka@gmail.com>
2018-02-15 17:25:57 +0200

    commit 1

---
+++ src/main/java/Chapter_1/Exampl.java
@@ -0,0 +1,7 @@
+package Chapter_1;
+
+public class Exampl {
+    public static void main(String[] args) {
+        System.out.println(" Джава правит Интернетом!!!");
+    }
+}
---
+++ src/main/java/Chapter_1/Exampl_2.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+public class Exampl_2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+        var1 = 1024;
+        System.out.println("Перемення var1 содержит " + var1);
+                var2 = var1 / 2;
+        System.out.println("Переменная var2 содержит var1/2" + var1/2);
+        System.out.println(var2);
+
+    }
+}
